4ccf38f4597aa203c126ac0029696121a52791dc
Tanzeem Ahmad <ahmadtanzeem185@gmail.com>
2026-02-07 11:03:51 +0530

    setup middlewares

---
+++ server/middlewares/errorMiddleware.js
@@ -0,0 +1,32 @@
+class ErrorHandler extends Error {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+};
+
+export const errorMiddleware = (err, req, res, next) => {
+    err.message = err.message || "Interal Server Error!";
+    err.statusCode = err.statusCode || 500;
+
+    if (err.code === 11000) {
+        const message = "Duplicate Field Value Entered!";
+        err = new ErrorHandler(message, 400);
+    }
+
+    if (err.name === "JsonWebTokenError") {
+        const message = "JSON Web Token has expired. try again!";
+        err = new ErrorHandler(message, 400);
+    }
+
+    const errorMessage = err.errors ? Object.values(err.errors).map((error) => error.message).join(" ") : err.message;
+    console.log("Error Middleware :", err);
+
+    return res.status(err.statusCode).json({
+        success: false,
+        message: errorMessage,
+    })
+
+}
+
+export default ErrorHandler;
