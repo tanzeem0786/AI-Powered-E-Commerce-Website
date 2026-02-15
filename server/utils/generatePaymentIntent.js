e7ce47ed5d689a567111448d4a9eba9d20cc23c2
Tanzeem Ahmad <ahmadtanzeem185@gmail.com>
2026-02-15 18:40:59 +0530

    complete Stripe Payment setup with api

---
+++ server/utils/generatePaymentIntent.js
@@ -0,0 +1,24 @@
+import Stripe from "stripe";
+import database from "../database/db.js";
+
+
+// 6:55
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+ 
+export async function generatePaymentIntent(orderId, totalPrice) {
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: totalPrice * 100 , // CONVERTING IN CENT
+            currency: "inr"
+        });
+        await database.query(
+            "INSERT INTO payments (order_id, payment_type, payment_status, payment_intent_id) VALUES ($1, $2, $3, $4) RETURNING *",
+            [orderId, "Online", "Pending", paymentIntent.client_secret]
+        );
+        return { success: true, clientSecret: paymentIntent.client_secret};
+
+    } catch (error) {
+        console.error("Payment Error: ", error.message || error);
+        return {success: false, message: "Payment Failed!"};
+    }
+}
